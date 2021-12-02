7cf55346ea4022964c3ba66bc5a6b29fb8651230
Dalon Work <dwwork@gmail.com>
2021-12-02 08:53:12 -0500

    Day1 Part2

--- src/day1.rs
+++ src/day1.rs
@@ -1,14 +1,7 @@
-
-
 fn main() {
-    let depths : Vec<i32> = include_str!("../inputs/day1.txt")
-        .lines()
-        .map(|line| line.parse().unwrap())
-        .collect();
-
-    let mut count = 0;
-    for w in depths.windows(2) {
-        count += (w[1] > w[0]) as i32;
-    }
-    println!("{}",count);
+    let depths: Vec<i32> = include_str!("../inputs/day1.txt").lines().map(|line| line.parse().unwrap()).collect();
+    let part1: i32 = depths.windows(2).map(|w| (w[1] > w[0]) as i32).sum();
+    println!("{}", part1);
+    let part2: i32 = depths.windows(3).map(|w| w.iter().sum()).collect::<Vec<i32>>().windows(2).map(|w| (w[1] > w[0]) as i32).sum();
+    println!("{}", part2);
 }
