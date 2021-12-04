1e51e1ef3a637cf8536385ff775cd0ad0a4bbead
Dalon Work <dwwork@gmail.com>
2021-12-03 20:22:02 -0500

    Day 2 Part 2

--- src/day2.rs
+++ src/day2.rs
@@ -1,17 +1,38 @@
 fn main() {
+    let dirs: Vec<(&str, i32)> = include_str!("../inputs/day2.txt")
+        .lines()
+        .map(|line| {
+            let mut it = line.split_whitespace();
+            (it.next().unwrap(), it.next().unwrap().parse().unwrap())
+        })
+        .collect();
+
     let mut x = 0;
     let mut y = 0;
-
-    for line in include_str!("../inputs/day2.txt").lines() {
-        let mut it = line.split_whitespace();
-        let dir : &str = it.next().unwrap();
-        let c : isize = it.next().unwrap().parse().unwrap();
-        match dir {
-            "forward" => x += c,
-            "down"    => y += c,
-            "up"      => y -= c,
-            _ => panic!("Bad direction!")
+    for d in &dirs {
+        match d.0 {
+            "forward" => x += d.1,
+            "down" => y += d.1,
+            "up" => y -= d.1,
+            _ => unreachable!(),
         }
     }
-    println!("(x,y) m: ({},{}) {}",x,y, x*y);
+    println!("Part 1 {}", x * y);
+
+    x = 0;
+    y = 0;
+    let mut a = 0;
+
+    for d in &dirs {
+        match d.0 {
+            "forward" => {
+                x += d.1;
+                y += a * d.1;
+            }
+            "down" => a += d.1,
+            "up" => a -= d.1,
+            _ => unreachable!(),
+        }
+    }
+    println!("Part 2 {}", x * y);
 }
