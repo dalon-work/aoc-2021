e267fe980b333e48658306a7eceaf642c90386e5
Dalon Work <dwwork@gmail.com>
2021-12-04 22:30:11 -0500

    Day 3, part 2

--- src/day3.rs
+++ src/day3.rs
@@ -12,17 +12,54 @@ fn part1(nbits : usize, lines : &[&str]) -> usize {
         }
     }
     
-    let mut gamma = 0usize;
+    let mut gamma = String::new();
+    let mut epsilon = String::new();
+
     for i in 0..nbits {
         if count[i] > 0 {
-            gamma ^= (1<<(nbits-i-1));
-        } else if count[i] == 0 {
-            panic!("This shouldn't happen!");
+            gamma.push('1');
+            epsilon.push('0');
+        } else {
+            gamma.push('0');
+            epsilon.push('1');
         }
     }
 
-    let mask : usize = (1 << nbits)-1;
-    gamma*(!gamma & mask)
+    let gamma = usize::from_str_radix(&gamma,2).unwrap();
+    let epsilon = usize::from_str_radix(&epsilon,2).unwrap();
+
+    gamma * epsilon
+}
+
+fn part2(nbits : usize, lines : &[&str], comp : fn(usize,usize) -> bool) -> usize {
+    let mut cur = lines.to_vec();
+    let mut bucket0 = Vec::<&str>::new();
+    let mut bucket1 = Vec::<&str>::new();
+
+    for i in 0..nbits {
+        bucket0.clear();
+        bucket1.clear();
+
+        for l in &cur {
+            if l.as_bytes()[i] == '1' as u8 {
+                bucket1.push(l);
+            } else {
+                bucket0.push(l);
+            }
+        }
+
+        if comp(bucket0.len(),bucket1.len()) {
+            std::mem::swap(&mut bucket0, &mut cur);
+        } else {
+            std::mem::swap(&mut bucket1, &mut cur);
+        }
+
+        if cur.len() == 1 {
+            break
+        }
+    }
+    assert!(cur.len() == 1);
+    return usize::from_str_radix(cur[0],2).unwrap();
 }
 
 
@@ -30,16 +67,23 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_example() {
+    fn test_part1() {
         let lines : Vec<&str> = include_str!("../inputs/day3.example.txt").lines().collect();
         assert_eq!(part1(5, &lines), 198);
+    }
+
+    #[test]
+    fn test_part2() {
+        let lines : Vec<&str> = include_str!("../inputs/day3.example.txt").lines().collect();
+        assert_eq!(part2(5, &lines, |l0,l1| l0 > l1), 23);
+        assert_eq!(part2(5, &lines, |l0,l1| l0 <= l1), 10);
     }
 }
 
 
-
-
 fn main() {
     let lines : Vec<&str> = include_str!("../inputs/day3.txt").lines().collect();
-    println!("Part 1: {}", part1(12,&lines));
+    println!("Part 1: {}", part1(12, &lines));
+    println!("Part 2: {}", part2(12, &lines, |l0,l1| l0 > l1) *
+                           part2(12, &lines, |l0,l1| l0 <= l1));
 }
