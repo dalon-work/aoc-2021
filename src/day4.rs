ecdd4d22289180ff25cd4845f5fa21a3c94bcc44
Dalon Work <dwwork@gmail.com>
2021-12-07 22:01:27 -0500

    Day 4, Part2 - Surprisingly difficult, especially with rust

--- src/day4.rs
+++ src/day4.rs
@@ -1,4 +1,6 @@
-fn check_done(b: &[[i32;5];5], i: usize, j: usize) -> bool {
+type Board = [[i32;5];5];
+
+fn check_done(b: &Board, i: usize, j: usize) -> bool {
     let mut row_done = true;
     let mut col_done = true;
     for k in 0..5 {
@@ -8,7 +10,7 @@ fn check_done(b: &[[i32;5];5], i: usize, j: usize) -> bool {
     return row_done || col_done;
 }
 
-fn sum(b: &[[i32;5];5]) -> i32 {
+fn sum(b: &Board) -> i32 {
     let mut s: i32 = 0;
     for i in 0..5 {
         for j in 0..5 {
@@ -20,7 +22,7 @@ fn sum(b: &[[i32;5];5]) -> i32 {
     return s;
 }
 
-fn part1(numbers: &[i32], boards: &[ [[i32;5];5] ]) -> i32 {
+fn part1(numbers: &[i32], boards: &[Board], ) -> i32 {
     let mut boards = boards.to_vec();
 
     for n in numbers {
@@ -40,9 +42,42 @@ fn part1(numbers: &[i32], boards: &[ [[i32;5];5] ]) -> i32 {
     return 0;
 }
 
+use std::collections::HashSet;
 
-fn parse_board(lines: &mut std::str::Lines) -> [[i32;5];5] {
-    let mut r: [[i32;5];5] = Default::default();
+fn part2(numbers: &[i32], boards: &[Board], ) -> i32 {
+    let mut boards = boards.to_vec();
+    let mut win = HashSet::new();
+    let mut order = Vec::new();
+
+    for n in numbers {
+        if win.len() == boards.len() {
+            break
+        }
+        for (bidx, b) in boards.iter_mut().enumerate() {
+            for i in 0..5 {
+                for j in 0..5 {
+                    if b[i][j] == *n {
+                        b[i][j] = -1;
+                        if check_done(&b, i, j) {
+                            if !win.contains(&bidx) {
+                                win.insert(bidx);
+                                order.push((bidx,*n));
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    let (last_board_idx, last_board_n) = order.last().unwrap();
+
+    return last_board_n*sum(&boards[*last_board_idx]);
+}
+
+
+fn parse_board(lines: &mut std::str::Lines) -> Board {
+    let mut r: Board = Default::default();
 
     for i in 0..5 {
         for (j, d) in lines.next().unwrap().split_whitespace().enumerate() {
@@ -56,8 +91,8 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_part1() {
-    let mut boards = Vec::<[[i32;5];5]>::new();
+    fn test_day4() {
+    let mut boards = Vec::<Board>::new();
     let mut lines = include_str!("../inputs/day4.example.txt").lines();
     let numbers: Vec<i32> = lines.next().unwrap()
         .split(',')
@@ -71,12 +106,13 @@ mod tests {
         }
     }
     assert_eq!(part1(&numbers, &boards), 4512);
+    assert_eq!(part2(&numbers, &boards), 1924);
     }
 }
 
 
 fn main() {
-    let mut boards = Vec::<[[i32;5];5]>::new();
+    let mut boards = Vec::<Board>::new();
     let mut lines = include_str!("../inputs/day4.txt").lines();
     let numbers: Vec<i32> = lines.next().unwrap()
         .split(',')
@@ -91,5 +127,6 @@ fn main() {
     }
 
     println!("Part 1 {}", part1(&numbers, &boards));
+    println!("Part 2 {}", part2(&numbers, &boards));
 
 }
