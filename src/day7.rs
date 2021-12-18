118e1d4eda35618e5c0e411592fd5adf860b05d4
Dalon Work <dwwork@gmail.com>
2021-12-17 20:51:52 -0500

    Day 7, Part 2

--- src/day7.rs
+++ src/day7.rs
@@ -1,4 +1,4 @@
-fn cost(crabs: &[i64], pos: i64) -> i64 {
+fn part1(crabs: &[i64], pos: i64) -> i64 {
     let mut fuel = 0;
     for c in crabs {
         fuel += (c-pos).abs();
@@ -6,7 +6,16 @@ fn cost(crabs: &[i64], pos: i64) -> i64 {
     return fuel
 }
 
-fn part1(crabs: &[i64]) -> i64 {
+fn part2(crabs: &[i64], pos: i64) -> i64 {
+    let mut fuel = 0;
+    for c in crabs {
+        let n = (c-pos).abs();
+        fuel += n*(n+1)/2
+    }
+    return fuel
+}
+
+fn run(crabs: &[i64], cost: fn(&[i64],i64)->i64 ) -> i64 {
     let mut m =i64::MAX;
     for pos in crabs[0]..=crabs[crabs.len()-1] {
         m = std::cmp::min(m, cost(crabs, pos));
@@ -17,5 +26,6 @@ fn part1(crabs: &[i64]) -> i64 {
 fn main() {
     let mut crabs: Vec<i64> = include_str!("../inputs/day7.txt").trim().split(",").map(|num| num.parse().unwrap()).collect();
     crabs.sort();
-    println!("{}",part1(&crabs));
+    println!("{}",run(&crabs, part1));
+    println!("{}",run(&crabs, part2));
 }
