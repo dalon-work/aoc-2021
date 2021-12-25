d092fac51520c3c6fb527a7a78b02a483ab8c52b
Dalon Work <dwwork@gmail.com>
2021-12-25 15:20:51 -0500

    Day 9, Part 2

--- src/day9.rs
+++ src/day9.rs
@@ -1,5 +1,6 @@
 use ndarray::Array2;
 use ndarray::ArrayView2;
+use ndarray::ArrayViewMut2;
 
 fn part1(height: ArrayView2<u32>) -> u32 {
 
@@ -17,6 +18,42 @@ fn part1(height: ArrayView2<u32>) -> u32 {
     return count;
 }
 
+fn floodfill(height: &mut ArrayViewMut2<u32>, i: usize, j:usize) -> u32 {
+
+    if height[[i,j]] == 9 {
+        return 0;
+    }
+
+    height[[i,j]] = 9;
+
+    let mut area = 1;
+
+    area += floodfill(height, i, j+1);
+    area += floodfill(height, i, j-1);
+    area += floodfill(height, i+1, j);
+    area += floodfill(height, i-1, j);
+
+    return area;
+}
+
+fn part2(mut height: ArrayViewMut2<u32>) -> u32 {
+
+    let mut basins = Vec::new();
+
+    for i in 1..=101 {
+        for j in 1..=101 {
+            if height[[i,j]] != 9 {
+                basins.push( floodfill(&mut height,i,j) );
+            }
+        }
+    }
+
+    basins.sort();
+    basins.reverse();
+
+    return basins[0] * basins[1] * basins[2];
+}
+
 fn main() {
     let mut lines = include_str!("../inputs/day9.txt").lines();
     let mut height = Array2::<u32>::from_elem((102,102),9);
@@ -28,4 +65,5 @@ fn main() {
     }
 
     println!("{}",part1(height.view()));
+    println!("{}", part2(height.view_mut()));
 }
