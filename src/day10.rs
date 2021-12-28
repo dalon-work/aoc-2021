8a2e61a38e8107ad23653ab8da5c68d76f7a98a4
Dalon Work <dwwork@gmail.com>
2021-12-27 22:56:30 -0500

    Day 10, Part 2

--- src/day10.rs
+++ src/day10.rs
@@ -1,84 +1,100 @@
-
 #[derive(PartialEq, Eq)]
-enum Bracket {
+enum Brace {
     Round,
     Square,
     Curly,
     Angle,
 }
 
+#[derive(Copy, Clone)]
 enum LineError {
-    Incomplete,
-    Corrupted(Bracket)
+    Incomplete(usize),
+    Corrupted(usize)
+}
+
+fn corrupted(b: Brace) -> LineError {
+    LineError::Corrupted(
+        match b {
+            Brace::Round => 3,
+            Brace::Square => 57,
+            Brace::Curly => 1197,
+            Brace::Angle => 25137,
+        }
+    )
+}
+
+fn incomplete(b: Brace, mut score: usize) -> LineError {
+    score *= 5;
+    LineError::Incomplete(score +
+        match b {
+            Brace::Round => 1,
+            Brace::Square => 2,
+            Brace::Curly => 3,
+            Brace::Angle => 4,
+        }
+    )
 }
 
 // processes an open bracket
-fn open(b: Bracket, ch: &mut std::str::Chars) -> Result<Bracket, LineError> {
+fn open(b: Brace, ch: &mut std::str::Chars) -> Result<Brace, LineError> {
 
-    // If we had an error, throw it up the stack
-    let pair: Bracket = parse_part1(ch)?;
+    let parsed = parse(ch);
 
-    if pair != b {
-        return Err(LineError::Corrupted(pair));
-    }
-
-    // If we made it here, we successfully matched a closing brace, so parse the next char.
-    return parse_part1(ch);
-}
-
-fn parse_part1(ch: &mut std::str::Chars) -> Result<Bracket, LineError> {
-    let c = ch.next();
-    if let Some(c) = c {
-        match c {
-            '(' => return open(Bracket::Round, ch),
-            '{' => return open(Bracket::Curly, ch),
-            '[' => return open(Bracket::Square, ch),
-            '<' => return open(Bracket::Angle, ch),
-            ')' => return Ok(Bracket::Round),
-            '}' => return Ok(Bracket::Curly),
-            ']' => return Ok(Bracket::Square),
-            '>' => return Ok(Bracket::Angle),
-            _ => unreachable!(),
+    match parsed {
+        Ok(closing_brace) => {
+            if closing_brace != b {
+                return Err(corrupted(closing_brace));
+            } else {
+                return parse(ch);
+            }
         }
-    } else { // We reached the end of the input
-        return Err(LineError::Incomplete);
-    }
-}
-
-fn part1(lines: &mut std::str::Lines) -> usize {
-    let mut score = 0usize;
-    for l in lines {
-        match parse_part1(&mut l.chars()) {
-            Ok(_) => {},
-            Err(e) => {
-                match e {
-                    LineError::Corrupted(b) => {
-                        match b {
-                            Bracket::Round => score += 3,
-                            Bracket::Square => score += 57,
-                            Bracket::Curly => score += 1197,
-                            Bracket::Angle => score += 25137,
-                        }
-                    }
-                    _ => {}
-                }
+        Err(err) => {
+            match err {
+                LineError::Incomplete(score) => {
+                    return Err(incomplete(b, score));
+                },
+                // pass corrupted back up the stack
+                LineError::Corrupted(_) => return parsed,
             }
         }
     }
-    return score;
 }
 
-mod test {
-    use super::*;
-
-    #[test]
-    fn test_day10() {
-        let mut lines = include_str!("../inputs/day10.example.txt").lines();
-        assert_eq!(part1(&mut lines), 26397);
+fn parse(ch: &mut std::str::Chars) -> Result<Brace, LineError> {
+    let c = ch.next();
+    if let Some(c) = c {
+        match c {
+            '(' => return open(Brace::Round, ch),
+            '{' => return open(Brace::Curly, ch),
+            '[' => return open(Brace::Square, ch),
+            '<' => return open(Brace::Angle, ch),
+            ')' => return Ok(Brace::Round),
+            '}' => return Ok(Brace::Curly),
+            ']' => return Ok(Brace::Square),
+            '>' => return Ok(Brace::Angle),
+            _ => unreachable!(),
+        }
+    } else { // We reached the end of the input
+        return Err(LineError::Incomplete(0));
     }
+}
+
+fn run(lines: &mut std::str::Lines) -> (usize,usize) {
+    let mut part1 = 0usize;
+    let mut part2 = Vec::new();
+    for l in lines {
+        if let Err(e) = parse(&mut l.chars()) {
+            match e {
+                LineError::Corrupted(score) => { part1 += score; },
+                LineError::Incomplete(score) => { part2.push(score) },
+            }
+        }
+    }
+    part2.sort();
+    return (part1, part2[part2.len()/2])
 }
 
 fn main() {
     let mut lines = include_str!("../inputs/day10.txt").lines();
-    println!("{}",part1(&mut lines));
+    println!("{:?}",run(&mut lines));
 }
