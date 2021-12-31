a6ea91c871540e5510e857772c2dbeb2efa7db8a
Dalon Work <dwwork@gmail.com>
2021-12-31 14:39:45 -0500

    Day 12, Part 2

--- src/day12.rs
+++ src/day12.rs
@@ -1,13 +1,16 @@
 use std::collections::HashMap;
-use std::collections::HashSet;
 
 // visited only contains the small caves
-fn part1_impl<'a>(caves: &HashMap<&str,Vec<&'a str>>, cur: &'a str, small: &mut Vec<&'a str>) -> usize {
+fn part1_impl<'a>(caves: &HashMap<&str,Vec<&'a str>>, cur: &'a str, small: &mut Vec<&'a str>, mut twice: bool) -> usize {
     let cur_neighbors = &caves[cur];
 
     // This small cave has already been visited
     if small.contains(&cur) {
-        return 0;
+        if twice {
+            return 0;
+        } else {
+            twice = true;
+        }
     }
 
     // We found a successful path through
@@ -26,7 +29,10 @@ fn part1_impl<'a>(caves: &HashMap<&str,Vec<&'a str>>, cur: &'a str, small: &mut 
     // Node hasn't been processed yet. Loop through neighbors and add up paths
     let mut count = 0;
     for c in cur_neighbors {
-        count += part1_impl(caves, c, small);
+        if *c == "start" { // Don't ever return to start
+            continue;
+        }
+        count += part1_impl(caves, c, small, twice);
     }
 
     // now pop self from the small list
@@ -39,7 +45,7 @@ fn part1_impl<'a>(caves: &HashMap<&str,Vec<&'a str>>, cur: &'a str, small: &mut 
 
 fn part1(caves: &HashMap<&str,Vec<&str>>) -> usize {
     let mut small = Vec::new();
-    let ans = part1_impl(caves, "start", &mut small);
+    let ans = part1_impl(caves, "start", &mut small, false);
     return ans;
 }
 
